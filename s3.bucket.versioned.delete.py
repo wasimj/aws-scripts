79fdc4597e2657f1652a9ba6203424b30ef22858
Wasim Juned <w@juned.net>
2017-07-07 11:49:06 +0100

    Delete versioned bucket

---
+++ s3.bucket.versioned.delete.py
@@ -0,0 +1,10 @@
+BUCKET = 'your-bucket-here'
+
+import boto3
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(BUCKET)
+bucket.object_versions.delete()
+
+# if you want to delete the now-empty bucket as well, uncomment this line:
+bucket.delete()
